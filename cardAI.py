0292b0d80963d801cf3452f77b5649f1e6c36073
“LeiliZadeh” <tuq66935@temple.edu>
2023-09-24 15:49:05 -0400

    first test

---
+++ cardAI.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+import time
+import google.generativeai as palm
+import openai
+#import PyPDF2
+import os
+#from pypdf import PdfReader
+
+# Configure the PaLM API key
+palm.configure(api_key='AIzaSyCXpiohNJcnIh8D4pv6w-3uVy8Bxo0n048')
+
+# Use the palm.list_models function to find available models:
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+model = models[0].name
+print(model)
+
+# Simplest Chatbot
+'''prompt1 = """
+You are an expert at solving word problems.
+
+Solve the following problem:
+
+I have three houses, each with three cats.
+each cat owns 4 mittens, and a hat. Each mitten was
+knit from 7m of yarn, each hat from 4m.
+How much yarn was needed to make all the items?
+
+Think about it step by step, and show your work.
+"""
+
+completion1 = palm.generate_text(
+    model=model,
+    prompt=prompt1,
+    temperature=0,
+    max_output_tokens=800,
+)
+
+print(completion1.result)
+
+# Set your input text
+prompt2 = "What is Quantum Computing? Explain like I'm 5."
+
+completion2 = palm.generate_text(
+    model=model,
+    prompt=prompt2,
+    temperature=0,
+    max_output_tokens=200,
+)
+
+print(completion2.result)
+'''
+# Custom Chatbot - Text Summarizer
+directory = '/content/drive/MyDrive/1-Build-a-Chatbot/'
+filename = 'Attention-Is-All-You-Need.pdf'
+
+# Create a pdf file object
+pdfFileObject = open(directory + filename, 'rb')
+# Create a pdf reader object
+pdfReader = PdfReader(pdfFileObject)
+text = []
+summary = ''
+
+# Storing the pages in a list
+for i in range(0, len(pdfReader.pages)):
+    # Creating a page object
+    pageObj = pdfReader.pages[i].extract_text()
+    pageObj = pageObj.replace('\t\r', '')
+    pageObj = pageObj.replace('\xa0', '')
+    # Extracting text from page
+    text.append(pageObj)
+
+# Merge multiple pages to reduce API Calls
+def join_elements(lst, chars_per_element):
+    new_lst = []
+    for i in range(0, len(lst), chars_per_element):
+        new_lst.append(''.join(lst[i:i+chars_per_element]))
+    return new_lst
+
+# Option to keep x elements per list element
+new_text = join_elements(text, 3)
+
+print(f"Original Pages = ", len(text))
+print(f"Compressed Pages = ", len(new_text))
+
+def get_completion(prompt):
+    completion = palm.generate_text(model=model,
+                                    prompt=prompt,
+                                    temperature=0,
+                                    max_output_tokens=200,
+                                    )
+    return completion.result
+
+summary = ""
+for i in range(len(new_text)):
+    prompt = f"""
+  Your task is to act as a Text Summariser.
+  I'll give you text from  pages of a book from beginning to end.
+  And your job is to summarise text from these pages in less than 100 words.
+  Don't be conversational. I need a plain 100 word answer.
+  Text is shared below, delimited with triple backticks:
+  ```{text[i]}```
+  """
+    try:
+        response = get_completion(prompt)
+    except:
+        response = get_completion(prompt)
+    print(response)
+    summary = f"{summary} {response}\n\n"
+    time.sleep(19)  # You can query the model only 3 times in a minute for free, so we need to put some delay
+
+with open(directory + '/palm_api_summary.txt',
+          'w') as out:
+    out.write(summary) 
+
